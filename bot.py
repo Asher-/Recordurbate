63be0a132b93b960690149b163c0762116a602dc
Asher Haig <asher@strong.ai>
2025-06-19 09:18:42 -0400

    Removed is_online function and rewrote handling of subprocess to check if ffmpeg failed due to streamer being offline

--- bot.py
+++ bot.py
@@ -3,6 +3,9 @@ import signal
 import subprocess
 import time
 
+import json
+from pprint import pprint
+
 import requests
 from config import load_config
 
@@ -65,36 +68,6 @@ class Bot:
             if not found:
                 self.config["streamers"].append([new_streamer, False])
 
-    def is_online(self, username):
-        # Fixes issue #69 "Failed when calling is_online(..), new API url available."
-        # See the link below for full list of API parameters (gender, region, tag, limit, offset, etc..)
-        # Official Chaturbate API https://chaturbate.com/affiliates/promotools/api_usersonline/
-        # Special thanks to https://www.blackhatworld.com/seo/chaturbate-api.1028000/page-2#post-11041420
-        
-        # With this API url, cam username must be in the first 500 results in order to be current_show="public" to be verified, due to max limit=500
-        # offset=(any non-negative number) can be included to obtain more results beyond the first 500.
-        MAX_API_RESULTS = "500" 
-        url = "https://chaturbate.com/api/public/affiliates/onlinerooms/?wm=DkfRj&client_ip=request_ip&limit=" + MAX_API_RESULTS
-
-        try:
-            time.sleep(3)  # fix issue 30
-            r = requests.get(url)
-            results =  r.json()["results"]
-            
-            self.logger.debug(r)
-            self.logger.debug(results)
-            
-            for result in results:
-                if result["username"] == username and result["current_show"] in ["public"]:
-                    return True
-
-            return False
-
-        except Exception as e:
-            self.logger.exception("Exception: call to is_online(..) failed.")
-            self.logger.exception(e)
-            return None
-
     def run(self):
         while self.running:
             
@@ -128,18 +101,21 @@ class Bot:
                         continue
 
                     # check if online
-                    if self.is_online(streamer[0]):
-                        self.logger.info("Started to record {}".format(streamer[0]))
 
-                        # prep args (dl bin and config)
-                        args = self.config["youtube-dl_cmd"].split(" ") + ["https://chaturbate.com/{}/".format(streamer[0]), "--config-location", self.config["youtube-dl_config"]] 
+                    # prep args (dl bin and config)
+                    args = self.config["youtube-dl_cmd"].split(" ") + ["https://chaturbate.com/{}/".format(streamer[0]), "--config-location", self.config["youtube-dl_config"]] 
                         
-                        # append idx and process to processes list
-                        self.processes.append([streamer[0], subprocess.Popen(args, 0)])
-
+                    # append idx and process to processes list
+                    process = subprocess.Popen(args, 0, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+                    time.sleep(1)
+                    if process.poll() is None:
+                        self.logger.info("Started to record {}".format(streamer[0]))
+                        self.processes.append([streamer[0], process])
                         # set to recording
                         self.config["streamers"][idx][1] = True
-                    
+                    #else:
+                    #    self.logger.info( streamer[0] + " is not currently online.")
+
                     # check rate limit
                     if self.config["rate_limit"]:
                         time.sleep(self.config["rate_limit_time"])
